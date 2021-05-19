0fa92111e62a2056f247c6ebbc8c8ea6273354f5
Jayasurya Karadi <70327304+jayasuryakaradi@users.noreply.github.com>
2021-05-19 10:38:56 +0530

    Add files via upload

---
+++ google_assis.js
@@ -0,0 +1,9 @@
+<!--led room1-->
+https://cloud.boltiot.com/remote/yourAPIKEY/digitalWrite?pin=0&state=HIGH&deviceName=yourBOLTID
+https://cloud.boltiot.com/remote/yourAPIKEY/digitalWrite?pin=0&state=LOW&deviceName=yourBOLTID
+
+
+
+<!--led room2-->
+https://cloud.boltiot.com/remote/yourAPIKEY/digitalWrite?pin=1&state=HIGH&deviceName=yourBOLTID
+https://cloud.boltiot.com/remote/yourAPIKEY/digitalWrite?pin=1&state=LOW&deviceName=yourBOLTID
---
+++ readme.txt
@@ -0,0 +1,2 @@
+for complete information visit
+https://www.hackster.io/jayasuryakaradi007/home-automation-using-google-assistant-and-bolt-app-de2c30
